2910dd2fed567807fcb7d019307068c969dfe090
sbarria92 <72055343+sbarria92@users.noreply.github.com>
2020-10-05 18:33:27 -0500

    Innitial commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/* JQUERY */
+/* SLICK CAROUSEL */
+
+$(document).ready(function () {
+    $('.carousel-container').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        mobileFirst:true,
+        autoplaySpeed: 6000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+})
+
+
+/* FUNCTIONS */
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', ()=> {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`
+            }
+        });
+
+        // Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+    
+}
+
+navSlide();
+
+
